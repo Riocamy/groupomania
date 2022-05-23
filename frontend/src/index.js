3dfda3c553956240442fc8bb6a2180e07f6e389f
Chlodion Gossart <chlodiongossart@gmail.com>
2022-05-24 00:23:30 +0200

    updated index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>,
+
+document.getElementById('root')
 );
