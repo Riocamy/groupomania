9a5086f395fafc64cf1efcbe5f7ed76d6a8a02fe
Chlodion Gossart <chlodiongossart@gmail.com>
2022-05-26 21:44:21 +0200

    added delete comment social component

---
+++ frontend/src/social/DeleteCom.js
@@ -0,0 +1,38 @@
+import React from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
+
+// Fonction pour supprimer un commentaire
+function DeleteCom(props) {
+  let urlElements = window.location.href.split("/");
+  let id = urlElements[4];
+
+  const deleteHandler = (e) => {
+    e.preventDefault();
+
+    let idCom = props.idCom;
+    const auth = Cookies.get("Token");
+
+    axios
+      .delete(`http://localhost:8080/api/comment/delete/${idCom}/${id}`, {
+        headers: {
+          Authorization: `${auth}`,
+        },
+      })
+      .then((response) => {
+        window.location = `/main/${id}`;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // Intégration dans le DOM
+  return (
+    <div>
+      <i className="fa fa-trash" onClick={deleteHandler}></i>
+    </div>
+  );
+}
+
+export default DeleteCom;
